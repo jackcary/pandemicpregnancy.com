4aea1bd9b2555cee6de2033672a1f96b96cedf61
Jack Cary <jack@jackcary.com>
2020-04-01 14:22:52 -0400

    update screenshot script to resize image

--- scripts/capture_screenshot.sh
+++ scripts/capture_screenshot.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-npm install --global capture-website-cli
-
+#capture screenshot using capture-website-cli
 capture-website https://pandemicpregnancy.com --width=1200 --height=630 --output=$HOME/pandemicpregnancy.com/static/img/pandemicpregnancy.png --overwrite
+
+sleep 2
+
+#resize using imagemagick (because the above height and width are not working)
+convert pandemicpregnancy.png -resize 1200x630 pandemicpregnancy.png
+
+
+
