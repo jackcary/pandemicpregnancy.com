dad4f7397db1d4991203bbc0573a356834927324
Jack Cary <jack@jackcary.com>
2020-04-01 18:25:36 -0400

    updates to capture screenshot script

--- scripts/capture_screenshot.sh
+++ scripts/capture_screenshot.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+#npm install --global capture-website-cli
+
 #capture screenshot using capture-website-cli
 capture-website https://pandemicpregnancy.com --width=1200 --height=630 --output=$HOME/pandemicpregnancy.com/static/img/pandemicpregnancy.png --overwrite
 
-sleep 2
+#sleep 2
 
 #resize using imagemagick (because the above height and width are not working)
-convert pandemicpregnancy.png -resize 1200x630 pandemicpregnancy.png
+#convert pandemicpregnancy.png -resize 1200x630 pandemicpregnancy.png
 
 
 
